87e28f92e8537a9b648603cc994ed6edde6355a6
Isbah Ansari <ansariisbah427@gmail.com>
2025-12-07 22:56:31 -0800

    unnecessary comments removed

--- twoSum.js
+++ twoSum.js
@@ -2,16 +2,14 @@ let arr = [3,2,4];
 target = 6;
 
 function twoSum (arr, target){
-    // let sumNum = [];
-    for (let i=0; i<arr.length; i++){
+        for (let i=0; i<arr.length; i++){
         
         for(let j=i+1; j<arr.length; j++){
             if(target-arr[i] === arr[j]) return [i,j];
         }
 
     }
-    // return sumNum;
+    
 }
 
 console.log(twoSum(arr, target))
-// twoSum(arr, target)
